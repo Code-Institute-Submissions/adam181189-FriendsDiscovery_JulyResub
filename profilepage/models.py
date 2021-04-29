8ed24101f8a5fdf2eb619b3116fec91293637fe7
adam181189 <adam181189@gmail.com>
2021-04-29 13:34:54 +0000

    set up paginator so only three posts are visible but can go through pages to see the rest

--- profilepage/models.py
+++ profilepage/models.py
@@ -4,13 +4,14 @@ from django.contrib.auth.models import User
 from django.conf import settings
 from users.models import UserDetails
 
+
 class Post(models.Model):
     #fields here
     new_post = models.TextField(max_length=1000)
     date_posted = models.DateTimeField(default=timezone.now)
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     userdetails = UserDetails
-    ## select users_userdetails.user_id == auth_user.id
 
     def __str__(self):
         return self.new_post[:10]
--- profilepage/views.py
+++ profilepage/views.py
@@ -3,18 +3,25 @@ from .forms import PostForm
 from .models import Post
 from users.models import UserDetails
 from django.contrib.auth.models import User
+from django.core.paginator import Paginator
 
 
 # Create your views here.
 def userprofile(request):
+
     """ A view to return the users profile page """
 
     userinfo = UserDetails.objects.get(user=User.objects.get(
         username=request.user.username))
 
     data = Post.objects.all().order_by('date_posted').reverse()
+    paginator = Paginator(data, 3)
 
-    context = {'userinfo': userinfo, 'data': data}
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
+    context = {
+        'userinfo': userinfo, 'data': data, 'page_obj': page_obj}
     return render(request, "profilepage/userprofile.html", context)
 
 
