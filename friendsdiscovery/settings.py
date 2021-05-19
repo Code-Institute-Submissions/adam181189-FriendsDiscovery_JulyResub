c6cf30738042508c2380b14bc6638cc07545cd47
adam181189 <adam181189@gmail.com>
2021-05-19 22:15:11 +0000

    add STATIC_ROOT config

--- friendsdiscovery/settings.py
+++ friendsdiscovery/settings.py
@@ -175,7 +175,6 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
-#STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 #STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
@@ -185,6 +184,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
