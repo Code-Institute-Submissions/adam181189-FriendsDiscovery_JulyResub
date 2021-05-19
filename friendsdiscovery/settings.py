6ba4ccaaaf50aeeb2b6613b6f4353ef16219097f
adam181189 <adam181189@gmail.com>
2021-05-19 20:14:29 +0000

    changes to settings.py for aws storages issue

--- friendsdiscovery/settings.py
+++ friendsdiscovery/settings.py
@@ -135,8 +135,8 @@ if 'USE_AWS' in os.environ:
     #Bucket config
     AWS_STORAGE_BUCKET_NAME = 'friendsdiscovery'
     AWS_S3_REGION_NAME = 'eu-west-2'
-    AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
-    AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
+    AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+    AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 
     # Static and media files
