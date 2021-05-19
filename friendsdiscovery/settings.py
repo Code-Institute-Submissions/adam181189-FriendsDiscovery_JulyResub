b6da8a95cf4e398061e2a93e618a1b02c48c2b1e
adam181189 <adam181189@gmail.com>
2021-05-19 22:27:04 +0000

    change STATIC URL/ROOT config

--- friendsdiscovery/settings.py
+++ friendsdiscovery/settings.py
@@ -17,8 +17,8 @@ from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 #BASE_DIR = Path(__file__).resolve().parent.parent
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
@@ -171,6 +171,7 @@ USE_TZ = True
 
 #STATIC_URL = '/static/'
 #STATIC_ROOT = 'static/'
+#STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 #STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 # Static files (CSS, JavaScript, Images)
@@ -182,9 +183,13 @@ USE_TZ = True
 #    os.path.join(BASE_DIR, 'static'),
 #)
 
+#STATIC_URL = '/static/'
+#STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
