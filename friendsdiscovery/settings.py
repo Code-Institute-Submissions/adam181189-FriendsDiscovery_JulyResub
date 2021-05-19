d56c1203420b13369e40ce48591b3676cf793457
adam181189 <adam181189@gmail.com>
2021-05-19 21:54:49 +0000

    further update AWS setting

--- friendsdiscovery/settings.py
+++ friendsdiscovery/settings.py
@@ -139,8 +139,6 @@ if 'USE_AWS' in os.environ:
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 
-    MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
-
     # Static and media files
     STATICFILES_STORAGE = 'custom_storages.StaticStorage'
     STATICFILES_LOCATION = 'static'
