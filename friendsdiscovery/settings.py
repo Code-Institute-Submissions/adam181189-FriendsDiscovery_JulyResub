56505dd2e88b57f95e1aa43d9061bd6ac4b536bb
adam181189 <adam181189@gmail.com>
2021-07-17 11:00:36 +0000

    set Debug to false

--- friendsdiscovery/settings.py
+++ friendsdiscovery/settings.py
@@ -22,11 +22,11 @@ from os.path import dirname, abspath
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY =   'SECRET_KEY' in os.environ # config('SECRET_KEY') #
+SECRET_KEY = 'SECRET_KEY' in os.environ # config('SECRET_KEY') #
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
-DEBUG = 'DEVELOPMENT' in os.environ #config('DEBUG') #
+DEBUG = 'FALSE' #config('DEBUG') #
 
 ALLOWED_HOSTS = ['friends-discovery.herokuapp.com', 'localhost']
 
